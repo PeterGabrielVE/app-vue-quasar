214c080d7fbaf1bf230a5e79cd5b893b3e5eab67
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-21 11:06:19 -0400

    crear rooutes en file independient

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -38,31 +38,11 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import EssentialLink from 'components/EssentialLink.vue';
+import { linksList } from '../router/link-list';
 
 defineOptions({
   name: 'MainLayout',
 });
-
-const linksList = [
-  {
-    title: 'Typography',
-    caption: 'Tipos de letras en Quasar',
-    icon: 'las la-align-left',
-    link: 'typography',
-  },
-  {
-    title: 'Flex Layout',
-    caption: 'Estilos con Flex',
-    icon: 'las la-layer-group',
-    link: 'flex',
-  },
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'las la-graduation-cap',
-    link: 'https://quasar.dev',
-  },
-];
 
 const leftDrawerOpen = ref(false);
 
