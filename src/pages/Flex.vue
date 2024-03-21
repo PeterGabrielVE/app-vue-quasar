e892351fb8c72e5f2fa2f4f1e4b6b2ad6350733c
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-21 10:51:07 -0400

    first commit

---
+++ src/pages/Flex.vue
@@ -0,0 +1,14 @@
+<template>
+  <q-page class="flex">
+    <h1>Flex.vue</h1>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Flex',
+});
+</script>
---
+++ src/pages/Typography.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page class="flex column q-pa-md">
+    <span class="text-h1">Headline 1</span>
+    <span class="text-h2">Headline 2</span>
+    <span class="text-h3">Headline 3</span>
+    <span class="text-h4">Headline 4</span>
+    <span class="text-h5">Headline 5</span>
+    <span class="text-h6">Headline 6</span>
+
+    <q-separator />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Typography',
+});
+</script>
