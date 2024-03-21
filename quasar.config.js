212d6e8065d6b7337673dbecc54648f01701f411
Pedro Gabriel <gaboleal123@gmail.com>
2024-03-21 16:51:48 -0400

    desactivando eslint

--- quasar.config.js
+++ quasar.config.js
@@ -70,9 +70,9 @@ module.exports = configure(function (/* ctx */) {
             vueTsc: {
               tsconfigPath: 'tsconfig.vue-tsc.json',
             },
-            eslint: {
-              lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"',
-            },
+            //eslint: {
+            // lintCommand: 'eslint "./**/*.{js,ts,mjs,cjs,vue}"',
+            //},
           },
           { server: false },
         ],
